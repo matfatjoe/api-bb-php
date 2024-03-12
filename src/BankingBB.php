cee5365cafa3e3937920e639d7b12fa68f597975
Furquim <matheus.fdecamargo@gmail.com>
2024-03-12 14:13:05 -0300

    [LISTAR BOLETOS] Ajuste no envio do filtro para listar boletos

--- src/BankingBB.php
+++ src/BankingBB.php
@@ -311,6 +311,7 @@ class BankingBB
 
     public function listarBoletos($filters)
     {
+        $filters['gw-dev-app-key'] = $this->config['application_key'];
         try {
             $response = $this->clientCobranca->request(
                 'GET',
@@ -321,17 +322,7 @@ class BankingBB
                         'Authorization' => 'Bearer ' . $this->token . ''
                     ],
                     'verify' => false,
-                    'query' => [
-                        'gw-dev-app-key' => $this->config['application_key'],
-                        'indicadorSituacao' => $filters['indicadorSituacao'],
-                        'agenciaBeneficiario' => $filters["agenciaBeneficiario"],
-                        'contaBeneficiario' => $filters["contaBeneficiario"],
-                        'cnpjPagador' => '',
-                        'digitoCNPJPagador' => '',
-                        'digitoCNPJPagador' => '',
-                        'codigoEstadoTituloCobranca' => $filters['codigoEstadoTituloCobranca'],
-                        'boletoVencido' => $filters['boletoVencido'],
-                    ],
+                    'query' => $filters,
                 ]
             );
             $statusCode = $response->getStatusCode();
